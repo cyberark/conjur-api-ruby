73253a03d301dcecae0ab62eedd6848a55fb40a0
Kevin Gilpin <kgilpin@gmail.com>
2013-04-08 15:11:59 -0400

    list groups

--- lib/conjur/api/groups.rb
+++ lib/conjur/api/groups.rb
@@ -2,6 +2,14 @@ require 'conjur/group'
 
 module Conjur
   class API
+    def groups
+      JSON.parse(RestClient::Resource.new(Conjur::Core::API.host, credentials)['groups'].get).collect do |json|
+        # TODO: remove this hack
+        json = JSON.parse json['json']
+        group(json['id'])
+      end
+    end
+
     def create_group(id, options = {})
       standard_create Conjur::Core::API.host, :group, id, options
     end
