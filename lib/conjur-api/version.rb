3573de4778c0790a71db61f75272933379244870
Rafał Rzepecki <divided.mind@gmail.com>
2013-06-04 23:29:48 +0200

    api#current_role method

--- lib/conjur-api/version.rb
+++ lib/conjur-api/version.rb
@@ -1,6 +1,6 @@
 module Conjur
   class API
-    VERSION = "2.3.1"
+    VERSION = "2.4.0"
     # Note: when bumping major version, please remove compatibility code in role#grant_to
   end
 end
--- lib/conjur/api/roles.rb
+++ lib/conjur/api/roles.rb
@@ -11,5 +11,13 @@ module Conjur
     def role role
       Role.new(Conjur::Authz::API.host, credentials)[self.class.parse_role_id(role).join('/')]
     end
+
+    def current_role
+      role_from_username username
+    end
+
+    def role_from_username username
+      role(username.split('/').unshift('user')[-2..-1].join(':'))
+    end
   end
 end
