e167da7df05a5149a4cd9bd7d852c774eb1bd5c5
Rafał Rzepecki <divided.mind@gmail.com>
2013-06-10 07:26:46 +0200

    resource specs

--- spec/lib/resource_spec.rb
+++ spec/lib/resource_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe Conjur::Resource do
   let(:account) { "the-account" }
   let(:uuid) { "ddd1f59a-494d-48fb-b045-0374c4a6eef9" }
-  
+
   context "identifier" do
     include Conjur::Escape
     let(:resource) { Conjur::Resource.new("#{Conjur::Authz::API.host}/#{account}/resources/#{kind}/#{path_escape identifier}") }
-    
+
     context "Object with an #id" do
       let(:kind) { "host" }
       let(:identifier) do
@@ -17,7 +17,7 @@ describe Conjur::Resource do
         resource.identifier.should == "foobar"
       end
     end
-    
+
     [ [ "foo", "bar/baz" ], [ "f:o", "bar" ], [ "@f", "bar.baz" ], [ "@f", "bar baz" ], [ "@f", "@:bar/baz" ] ].each do |p|
       context "of /#{p[0]}/#{p[1]}" do
         let(:kind) { p[0] }
@@ -31,6 +31,91 @@ describe Conjur::Resource do
           specify { should == ( p[1] ) }
         end
       end
+    end
+  end
+
+  let(:authz_host) { "http://authz.example.com" }
+  before { Conjur::Authz::API.stub host: authz_host }
+
+  let(:uri) { "#{authz_host}/some-account/resources/the-kind/resource-id" }
+  subject { Conjur::Resource.new uri }
+
+  describe '#create' do
+    it "simply puts" do
+      RestClient::Request.should_receive(:execute).with(
+        method: :put,
+        url: uri,
+        payload: {},
+        headers: {}
+      ).and_return "new resource"
+      subject.create.should == "new resource"
+    end
+  end
+
+  describe '#permitted_roles' do
+    it 'gets the list from /roles/allowed_to' do
+      RestClient::Request.should_receive(:execute).with(
+        method: :get,
+        url: "http://authz.example.com/some-account/roles/allowed_to/nuke/the-kind/resource-id",
+        headers: {}
+      ).and_return '["foo", "bar"]'
+
+      subject.permitted_roles("nuke").should == ['foo', 'bar']
+    end
+  end
+
+  describe '#give_to' do
+    it "puts the owner field" do
+      RestClient::Request.should_receive(:execute).with(
+        method: :put,
+        url: uri,
+        payload: {owner: 'new-owner' },
+        headers: {}
+      )
+
+      subject.give_to 'new-owner'
+    end
+  end
+
+  describe '#delete' do
+    it 'simply deletes' do
+      RestClient::Request.should_receive(:execute).with(
+        method: :delete,
+        url: uri,
+        headers: {}
+      )
+
+      subject.delete
+    end
+  end
+
+  describe '#permit' do
+    it 'posts permit for every privilege' do
+      privileges = [:nuke, :fry]
+      privileges.each do |p|
+        RestClient::Request.should_receive(:execute).with(
+          method: :post,
+          url: uri + "/?permit&privilege=#{p}&role=dr-strangelove",
+          headers: {},
+          payload: {}
+        )
+      end
+      subject.permit privileges, "dr-strangelove"
+    end
+  end
+
+  describe '#deny' do
+    it 'posts deny for every privilege' do
+      privileges = [:nuke, :fry]
+      privileges.each do |p|
+        RestClient::Request.should_receive(:execute).with(
+          method: :post,
+          url: uri + "/?deny&privilege=#{p}&role=james-bond",
+          headers: {},
+          payload: {}
+        )
+      end
+      subject.deny privileges, "james-bond"
     end
   end
 end
