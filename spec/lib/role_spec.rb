745e4a5b92ccdc957058270a05385b7b774fb7a4
Rafał Rzepecki <divided.mind@gmail.com>
2013-06-10 08:48:20 +0200

    More role specs.

--- spec/lib/role_spec.rb
+++ spec/lib/role_spec.rb
@@ -1,15 +1,16 @@
 require 'spec_helper'
 
-describe Conjur::Role do
+describe Conjur::Role, api: :dummy do
   let(:account) { "the-account" }
   let(:kind) { "test" }
-  let(:role) { Conjur::API.new_from_token({ 'data' => 'the-login' }).role([ account, kind, id ].join(":")) }
+  let(:url) { "#{authz_host}/#{account}/roles/#{kind}/#{id}" }
+  let(:role) { Conjur::Role.new url }
   subject { role }
 
   describe ".new" do
     context "with plain id" do
       let(:id) { "foo" }
-      its(:options) { should == {:headers=>{:authorization=>"Token token=\"eyJkYXRhIjoidGhlLWxvZ2luIn0=\""}, :username=>'the-login'} }
+      its(:options) {}
       its(:kind) { should == kind }
       its(:id) { should == id }
     end
@@ -52,6 +53,85 @@ describe Conjur::Role do
         members.should_receive(:put).with admin_option: true, foo: 'bar'
         subject.grant_to "other", true, foo: 'bar'
       end
+    end
+  end
+
+  describe '#create' do
+    it 'simply puts' do
+      RestClient::Request.should_receive(:execute).with(
+        method: :put,
+        url: url,
+        payload: {},
+        headers: {}
+      )
+      role.create
+    end
+  end
+
+  describe '#all' do
+    it 'returns roles for ids got from ?all' do
+      roles = [{'account' => 'foo', 'id' => 'k:bar'}, {'account' => 'baz', 'id' => 'k:xyzzy'}]
+      RestClient::Request.should_receive(:execute).with(
+        method: :get,
+        url: role.url + "/?all",
+        headers: {}
+      ).and_return roles.to_json
+      all = role.all
+      all[0].account.should == 'foo'
+      all[0].id.should == 'bar'
+      all[1].account.should == 'baz'
+      all[1].id.should == 'xyzzy'
+    end
+  end
+
+  describe '#revoke_from' do
+    it 'deletes member' do
+      RestClient::Request.should_receive(:execute).with(
+        method: :delete,
+        url: role.url + "/?members&member=the-member",
+        headers: {}
+      )
+      role.revoke_from 'the-member'
+    end
+  end
+
+  describe '#permitted?' do
+    before do
+      RestClient::Request.stub(:execute).with(
+        method: :get,
+        url: role.url + "/?check&resource_kind=chunky&resource_id=bacon&privilege=fry",
+        headers: {}
+      ) { result }
+    end
+
+    context "when get ?check is successful" do
+      let(:result) { :ok }
+      it "returns true" do
+        role.permitted?('chunky', 'bacon', 'fry').should be_true
+      end
+    end
+
+    context "when get ?check not found" do
+      let(:result) { raise RestClient::ResourceNotFound, 'foo' }
+      it "returns false" do
+        role.permitted?('chunky', 'bacon', 'fry').should be_false
+      end
+    end
+  end
+
+  describe '#members' do
+    it "gets ?members and turns each into RoleGrant" do
+      grants = %w(foo bar)
+      RestClient::Request.should_receive(:execute).with(
+        method: :get,
+        url: role.url + "/?members",
+        headers: {}
+      ).and_return grants.to_json
+      grants.each do |g|
+        Conjur::RoleGrant.should_receive(:parse_from_json).with(g, {}).and_return g
+      end
+
+      subject.members.should == grants
     end
   end
 end
