686aec06eee782907dfde43f1a3f175796e832d1
Hleb Rubanau <g.rubanau@gmail.com>
2014-07-01 13:29:47 +0300

    audit send: remove account from endpoint URL
    Fixes #32

--- lib/conjur/api/audit.rb
+++ lib/conjur/api/audit.rb
@@ -48,8 +48,7 @@ module Conjur
              else
                 raise ArgumentError, "Parameter should be either String, Hash or Array"
              end
-      rest_path = Conjur::Core::API.conjur_account + "/audit"
-      rest_api  = RestClient::Resource.new(Conjur::Authz::API.host, credentials)[rest_path]
+      rest_api  = RestClient::Resource.new(Conjur::Authz::API.host, credentials)["audit"]
       rest_api.post json, content_type: "text/plain"
     end
     
