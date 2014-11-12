1d0568b12b6f93c70f459c8f2747d4d051fda2c0
Jon Mason <jonathan.j.mason@gmail.com>
2014-11-12 14:53:25 -0600

    use new webmock syntax

--- spec/api/layer_spec.rb
+++ spec/api/layer_spec.rb
@@ -8,7 +8,9 @@ describe Conjur::Layer do
     it "casts Host to roleid" do
       host = double(:host)
       expect(host).to receive(:roleid).and_return "the-hostid"
-      stub_request(:post, "http://example.com/layers/my%2Flayername/hosts").with(hostid: "the-hostid")
+      stub_request(:post, "http://example.com/layers/my%2Flayername/hosts")
+        .with(body: {hostid: 'the-hostid' })
+
 
       subject.add_host host
     end
