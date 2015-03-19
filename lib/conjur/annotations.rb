ce144440547f02623b4476c87a659267bc49bbde
Jon Mason <jon@conjur.net>
2015-03-19 13:16:14 -0500

    doc and clean up Annotations

--- lib/conjur/annotations.rb
+++ lib/conjur/annotations.rb
@@ -19,13 +19,26 @@
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 module Conjur
+  # Conjur allows {http://developer.conjur.net/reference/services/authorization/resource Resource}
+  #   instances to be {http://developer.conjur.net/reference/services/authorization/resource/annotate.html annotated}
+  #   with arbitrary key-value data.  This data is generally for "user consumption", and it *is not* governed
+  #   by any particular schema or constraints.  Your applications can define their own schema for annotations they
+  #   use.  If you do so, we recommend prefixing your annotations, for example, `'myapp:Name'`, in order to avoid
+  #   conflict with annotations used, for example, by the Conjur  UI.
+  #
   # An Annotations instance acts like a Hash: you can fetch an annotation
-  # with '[]' and update with '[]=', 'each' it, and 'merge!' to do bulk updates
-  # (although merge! is not more efficient than setting each pair with '[]=').
+  #   with {#[]} and update with {#[]=}, {#each} it, and {#merge!} to do bulk updates.
+  #
   class Annotations
     include Enumerable
-    
-    # @api private
+
+    # Create an `Annotations` instance for the given {Conjur::Resource}.
+    #
+    # Note that you will generally use the {Conjur::Resource#annotations} method to get
+    # the `Annotations` for a {Conjur::Resource}.
+    #
+    # @param resource [Conjur::Resource]
+    #
     def initialize resource
       @resource = resource
     end
@@ -37,11 +50,15 @@ module Conjur
       annotations_hash[name.to_sym]
     end
     
-    # Set an annotation value
+    # Set an annotation value.  This will perform an api call to set the annotation
+    #   on the server.
+    #
     # @param [String, Symbol] name the annotation name
     # @param [String] value the annotation value
+    #
+    # @return [String] the new annotation value
     def []= name, value
-      update_annotation name, value
+      update_annotation name.to_sym, value
       value
     end
     
@@ -52,31 +69,48 @@ module Conjur
       self
     end
 
-    # Set annotations from key,value pairs in +hash+
-    # @param [#each] hash
+    # Set annotations from key,value pairs in `hash`.
+    #
+    # @note this is currently no more efficient than setting each
+    #   annotation with {#[]=}.
+    #
+    # @param [Hash, #each] hash
     # @return [Conjur::Annotations] self
     def merge! hash
       hash.each{|k,v| self[k] = v }
       self
     end
     
-    # Return a proper hash containing a +copy+ of ourself.  Note that
+    # Return a proper hash containing a **copy** of the annotations.  Note that
     # updates to this hash have no effect on the actual annotations.
+    #
     # @return [Hash]
     def to_h
       annotations_hash.dup
     end
-    
+
+    # Return the annotations hash as a string.  This method simply delegates to
+    # `Hash#to_s`.
+    #
+    # @return [String]
     def to_s
       annotations_hash.to_s
     end
-    
+
+    # Return an informative representation of the annotations, including
+    # the resource to which they're attached.  Suitable for debugging.
+    #
+    # @return [String]
     def inspect
       "<Annotations for #{@resource.resourceid}: #{to_s}>"
     end
     
     protected
-    
+    #@api private
+    # Update an annotation on the server.
+    # @param name [String]
+    # @param value [String, #to_s]
+    # @return [void]
     def update_annotation name, value
       @resource.invalidate do
         @annotations_hash = nil
@@ -84,11 +118,15 @@ module Conjur
         RestClient::Resource.new(Conjur::Authz::API.host, @resource.options)[path].put name: name, value: value
       end
     end
-    
+
+    # @api private
+    # Our internal {Hash} of annotations.  Lazily loaded.
     def annotations_hash
       @annotations_hash ||= fetch_annotations
     end
-    
+
+    # @api private
+    # Fetch the annotations from the server.
     def fetch_annotations
       {}.tap do |hash|
         @resource.attributes['annotations'].each do |annotation|
