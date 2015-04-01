f16fdd9027092b0b525602930671e59f309a3a6a
Jon Mason <jon@conjur.net>
2015-04-01 15:41:08 -0500

    doc roles api methods

--- lib/conjur/api/roles.rb
+++ lib/conjur/api/roles.rb
@@ -47,7 +47,7 @@ module Conjur
       Conjur::Graph.new RestClient::Resource.new(Conjur::Authz::API.host, credentials)["#{Conjur.account}/roles?#{query}"].get
     end
 
-    # Create a {Conjur::Role} with the given id
+    # Create a {Conjur::Role} with the given id.
     #
     # ### Permissions
     # * All Conjur roles can create new roles.
@@ -85,19 +85,70 @@ module Conjur
     # Return a {Conjur::Role} representing a role with the given id.  Note that the {Conjur::Role} may or
     # may not exist (see {Conjur::Exists#exists?}).
     #
+    # ### Permissions
+    # Because this method returns roles that may or may not exist, it doesn't require any permissions to call it:
+    # in fact, it does not perform an HTTP request (except for authentication if necessary).
     #
+    # @example Create and show a role
+    #   api.create_role 'cat:iggy'
+    #   iggy = api.role 'cat:iggy'
+    #   iggy.exists? # true
+    #   iggy.members.map(&:member).map(&:roleid) # => ['conjur:user:admin']
+    #   api.current_role.roleid # => 'conjur:user:admin' # creator role is a member of created role.
+    #
+    # @example No permissions are required to call this method
+    #   api.current_role # => "user:no-access"
+    #
+    #   # current role is only a member of itself, so it can't see other roles.
+    #   api.current_role.memberships.count # => 1
+    #   admin = api.role 'user:admin' # OK
+    #   admin.exists? # => true
+    #   admin.members # => RestClient::Forbidden: 403 Forbidden
+    #
+    # @param [String] role the id of the role, which must contain at least kind and id tokens (account is optional).
+    # @return [Conjur::Role] an object representing the role
     def role role
       Role.new(Conjur::Authz::API.host, credentials)[self.class.parse_role_id(role).join('/')]
     end
 
+    # Return a {Conjur::Role} object representing the role (typically a user or host) that this api is authenticated
+    # as.  This is derived either from the `login` argument to {Conjur::API.new_from_key} or from the contents of the
+    # `token` given to {Conjur::API.new_from_token}.
+    #
+    # @example Current role for a user
+    #   api = Conjur::API.new_from_key 'jon', 'somepassword'
+    #   api.current_role.roleid # => 'conjur:user:jon'
+    #
+    # @example Current role for a host
+    #   host = api.create_host id: 'exapmle-host'
+    #
+    #   # Host and User have an `api` method that returns an api with their credentials.  Note
+    #   # that this only works with a newly created host or user, which has an `api_key` attribute.
+    #   host.api.current_role.roleid # => 'conjur:host:example-host'
+    #
+    # @return [Conjur::Role] the authenticated role for this API instance
     def current_role
       role_from_username username
     end
 
+
+    #@!endgroup
+
+    # @api private
+    #
+    # Get a Role instance from a username or host id
+    # @param [String] username the username or host id
+    # @return [Conjur::Role]
     def role_from_username username
       role(role_name_from_username username)
     end
 
+    # @api private
+    #
+    # Convert a username or host id to a role identifier.
+    # This handles conversion of logins like 'host/foo' to 'host:foo'
+    # @param [String] username the user name or host id
+    # @return [String] A full role id for the user or host
     def role_name_from_username username = self.username
       tokens = username.split('/')
       if tokens.size == 1
@@ -107,9 +158,13 @@ module Conjur
       end
     end
 
-    #@!endgroup
-
     private
+
+    # @api  private
+    # Use of this method is deprecated in favor of Conjur::Cast#cast
+    # @deprecated
+    # @param [String, Conjur::Role] role object to extract a roel id from
+    # @return [String] the role id
     def normalize_roleid role
       case role
         when String then role
