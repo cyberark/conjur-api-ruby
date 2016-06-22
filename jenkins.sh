07217f5f333d71d5959e40365e2c7c9852fbce33
Alan Potter <alanjpotter@gmail.com>
2016-06-22 12:50:54 -0400

    test against 4.8

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-CONJUR_VERSION=${CONJUR_VERSION:-"5.0"}
+CONJUR_VERSION=${CONJUR_VERSION:-"4.8"}
 DOCKER_IMAGE=${DOCKER_IMAGE:-"registry.tld/conjur-appliance-cuke-master:$CONJUR_VERSION-stable"}
 NOKILL=${NOKILL:-"0"}
 PULL=${PULL:-"1"}
