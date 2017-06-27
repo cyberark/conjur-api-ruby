9281d1c3e96f61e0d0d97ad24c857f387e52e12d
Alan Potter <alanjpotter@gmail.com>
2017-06-27 11:51:34 -0400

    clean up test script a little

--- test.sh
+++ test.sh
@@ -1,31 +1,26 @@
 #!/bin/bash -ex
 
-docker build -t api-test -f Dockerfile.test .
-image_id=api-test
+test_image=api-test
+docker build -t $test_image -f Dockerfile.test .
 
 function finish {
 	docker rm -f $pg_cid
 	docker rm -f $server_cid
-    if [ ! -z "$KEEP_IMAGE" ]; then
-      docker rmi $image_id
-    fi
 }
 trap finish EXIT
 
 possum_tag=push-image_170626_0.1.0
-possum=registry.tld/possum:${possum_tag}
+possum_image=registry.tld/possum:$possum_tag
 
-export POSSUM_DATA_KEY="$(docker run --rm ${possum} data-key generate)"
+export POSSUM_DATA_KEY="$(docker run --rm $possum_image data-key generate)"
 
 pg_cid=$(docker run -d postgres:9.3)
-
-mkdir -p spec/reports
 
 server_cid=$(docker run -d \
 	--link $pg_cid:pg \
 	-e DATABASE_URL=postgres://postgres@pg/postgres \
 	-e RAILS_ENV=test \
-	${possum} server)
+	$possum_image server)
 
 admin_api_key=( $(cat ci/setup-account.sh | docker exec -i $server_cid /bin/bash | tail -1) )
 
@@ -43,5 +38,5 @@ docker run \
 	-e CONJUR_APPLIANCE_URL=http://possum \
 	-e CONJUR_ACCOUNT=cucumber \
     -e CONJUR_AUTHN_API_KEY=${admin_api_key[3]} \
-    $image_id /bin/bash ci/test.sh "$@"
+    $test_image ci/test.sh "$@"
 
