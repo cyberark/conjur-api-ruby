5c32d65e5d4cd061541bf9b56e2bf6b2775765c5
Jason Vanderhoof <jvanderhoof@gmail.com>
2017-07-13 12:32:31 -0600

    add `-T` command

--- test.sh
+++ test.sh
@@ -14,7 +14,7 @@ docker-compose up -d
 
 sleep 5
 # Generate a new account `cucumber` and extract the API key so we can log in for CI tests
-docker-compose exec possum possum account create cucumber >> tmp.txt
+docker-compose exec -T possum possum account create cucumber >> tmp.txt
 
 api_key=$(cat tmp.txt | tail -1 | awk '{print $4}' | tr -d '\r')
 
