1bbf91dcc03ae3f368af3d688df56eaddb160678
Jason Vanderhoof <jvanderhoof@gmail.com>
2017-07-13 10:37:04 -0600

    Updated test script to use docker compose cluster

--- test.sh
+++ test.sh
@@ -1,43 +1,23 @@
 #!/bin/bash -ex
 
-test_image=api-test
-docker build -t $test_image .
-
 function finish {
-	docker rm -f $pg_cid
-	docker rm -f $server_cid
+  docker-compose down
 }
 trap finish EXIT
 
-possum_tag=0.1.0-stable
-possum_image=registry.tld/possum:$possum_tag
-
-export POSSUM_DATA_KEY="$(docker run --rm $possum_image data-key generate)"
-
-pg_cid=$(docker run -d postgres:9.3)
-
-server_cid=$(docker run -d \
-	--link $pg_cid:pg \
-	-e DATABASE_URL=postgres://postgres@pg/postgres \
-	-e RAILS_ENV=test \
-	$possum_image server)
-
-set -o pipefail
-admin_api_key=( $(cat ci/setup-account.sh | docker exec -i $server_cid /bin/bash | tail -1) )
-
+# Generate reports folders locally
 mkdir -p spec/reports features/reports
 
-docker run \
-	-i \
-	--rm \
-	--link $pg_cid:pg \
-	--link $server_cid:possum \
-    -v $PWD/spec/reports:/src/conjur-api/spec/reports \
-    -v $PWD/features/reports:/src/conjur-api/features/reports \
-	-e DATABASE_URL=postgres://postgres@pg/postgres \
-	-e RAILS_ENV=test \
-	-e CONJUR_APPLIANCE_URL=http://possum \
-	-e CONJUR_ACCOUNT=cucumber \
-    -e CONJUR_AUTHN_API_KEY=${admin_api_key[3]} \
-    $test_image ci/test.sh "$@"
+# Build test container & start the cluster
+docker-compose build
+docker-compose up -d
 
+# provide time for all services to start
+# TODO once service startup check is working in Possum, we should use that check instead of just waiting.
+sleep 5
+
+# Generate a new account `cucumber` and extract the API key so we can log in for CI tests
+api_key=$(docker-compose exec possum possum account create cucumber | tail -1 | awk '{print $4}' | tr -d '\r')
+
+# Execute tests
+docker-compose exec tests env CONJUR_AUTHN_API_KEY="$api_key" bash -c 'ci/test.sh'
