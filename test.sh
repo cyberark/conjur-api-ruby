b82c6528f34b5577cb39493fe5da5c00bd6a64b8
Jason Vanderhoof <jvanderhoof@gmail.com>
2017-07-13 15:17:20 -0600

    Cleanup

--- test.sh
+++ test.sh
@@ -12,21 +12,13 @@ mkdir -p spec/reports features/reports
 docker-compose build
 docker-compose up -d
 
-# until [ $(docker-compose exec possum curl -X OPTIONS http://localhost) ]; do
-#   sleep 1
-# done
+# Delay to allow time for Possum to come up
+# TODO: remove this once we have HEALTHCHECK in place
 sleep 10
 
-# docker-compose exec -T possum possum account create cucumber
-
 api_key=$(docker-compose exec -T possum rails r "print Credentials['cucumber:user:admin'].api_key")
-echo "api_key: $api_key"
-# api_key=$(cat tmp.txt | tail -1 | awk '{print $4}' | tr -d '\r')
 
 # Execute tests
 docker-compose exec -T tests \
-  env CONJUR_AUTHN_API_KEY=$(docker-compose exec -T possum rails r "print Credentials['cucumber:user:admin'].api_key") \
+  env CONJUR_AUTHN_API_KEY="$api_key" \
   bash -c 'ci/test.sh'
-# env CONJUR_AUTHN_API_KEY=$(docker-compose exec -T possum rails r "print Credentials['cucumber:user:admin'].api_key") \
-#     docker-compose run tests ci/test.sh
-# $(docker-compose exec -T possum rails r "print Credentials['cucumber:user:admin'].api_key")
