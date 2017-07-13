558f53c67d6c52d0bd9f3253c532e1d26541c546
Jason Vanderhoof <jvanderhoof@gmail.com>
2017-07-13 12:25:18 -0600

    Push response to file to avoid pipes

--- test.sh
+++ test.sh
@@ -14,7 +14,9 @@ docker-compose up -d
 
 sleep 5
 # Generate a new account `cucumber` and extract the API key so we can log in for CI tests
-api_key=$(docker-compose exec possum possum account create cucumber | tail -1 | awk '{print $4}' | tr -d '\r')
+docker-compose exec possum possum account create cucumber >> tmp.txt
+
+api_key=$(cat tmp.txt | tail -1 | awk '{print $4}' | tr -d '\r')
 
 # Execute tests
 docker-compose exec tests env CONJUR_AUTHN_API_KEY="$api_key" bash -c 'ci/test.sh'
