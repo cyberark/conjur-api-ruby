5c60788931b5e5fbdeaedf2261a3e4056c09a0a4
Alex Kalish <alex.kalish@cyberark.com>
2019-05-16 16:41:34 -0400

    Provide improved inspect method
    Added a custom inspect method to the BaseObject class that simply prints
    the class name and the id, instead of the less readability version from
    Object.  As this is a superclass, all subclasses will now inherit this
    version of the inspect method.

---
+++ spec/base_object_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Conjur::BaseObject do
+
+  it "returns custom string for #inspect" do
+    id_str = 'foo:bar:baz'
+    base_obj = Conjur::BaseObject.new(Conjur::Id.new(id_str), { username: 'foo' })
+    expect(base_obj.inspect).to include("id='#{id_str}'")
+    expect(base_obj.inspect).to include(Conjur::BaseObject.name)
+  end
+
+end
