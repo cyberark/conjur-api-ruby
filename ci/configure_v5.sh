629c64d035fd5fd4d1f6fa7d3e902daefcdb7a59
Sasha Chernomordik <sashac@cyberark.com>
2021-12-08 10:48:04 +0200

    Wait more for up of Conjur v5

--- ci/configure_v5.sh
+++ ci/configure_v5.sh
@@ -3,11 +3,13 @@
 cat << "CONFIGURE" | docker exec -i $(docker-compose ps -q conjur_5) bash
 set -e
 
-for _ in $(seq 20); do
+for _ in $(seq 100); do
   curl -o /dev/null -fs -X OPTIONS http://localhost > /dev/null && break
-  echo .
+  echo -n .
   sleep 2
 done
+
+echo
 
 # So we fail if the server isn't up yet:
 curl -o /dev/null -fs -X OPTIONS http://localhost > /dev/null
