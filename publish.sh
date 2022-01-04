6d1f1247570bfb119fcbed0a993b4160460efc84
Matthew Brace <matthew.brace@cyberark.com>
2022-01-04 16:45:20 -0600

    Update to use release-tools publish-rubygem
    This moves away from the publish-rubygem container to use the
    release-tools provided publish-rubygem.  This allows the gem file
    to be archived along with the release rather than just on rubygems.org

--- publish.sh
+++ publish.sh
@@ -1,7 +1,5 @@
-#!/bin/bash -e
-
-docker pull registry.tld/conjurinc/publish-rubygem
+#!/usr/bin/env bash
+set -e
 
 summon --yaml "RUBYGEMS_API_KEY: !var rubygems/api-key" \
-  docker run --rm --env-file @SUMMONENVFILE -v "$(pwd)":/opt/src \
-  registry.tld/conjurinc/publish-rubygem conjur-api
+  publish-rubygem conjur-api
