77ceff8a7970120c7b22f3553d66cdcddf154376
Glen Johnson <glen.johnson@cyberark.com>
2023-12-15 08:42:34 -0700

    Update docker compose syntax

--- ci/configure_v4.sh
+++ ci/configure_v4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-cat << "CONFIGURE" | docker exec -i $(docker-compose ps -q conjur_4) bash
+cat << "CONFIGURE" | docker exec -i $(docker compose ps -q conjur_4) bash
 set -e
 
 /opt/conjur/evoke/bin/wait_for_conjur
@@ -9,4 +9,4 @@ evoke ca regenerate conjur_4
 env CONJUR_AUTHN_LOGIN=admin CONJUR_AUTHN_API_KEY=secret conjur policy load --as-group security_admin /etc/policy.yml
 CONFIGURE
 
-docker cp $(docker-compose ps -q conjur_4):/opt/conjur/etc/ssl/ca.pem ./tmp/conjur.pem
+docker cp $(docker compose ps -q conjur_4):/opt/conjur/etc/ssl/ca.pem ./tmp/conjur.pem
